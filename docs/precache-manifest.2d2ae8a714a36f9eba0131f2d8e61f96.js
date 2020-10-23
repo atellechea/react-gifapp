b4c0b40cf6fdc1b657a6e50ec1b69f4c7fe4f797
Adrian <atellechearascon@gmail.com>
2020-10-23 14:50:58 -0700

    Docs agregado

---
+++ docs/precache-manifest.2d2ae8a714a36f9eba0131f2d8e61f96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49cbc0e6268a033ccef79b8a1f2d9e87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a1f6b982c6feb6a9433",
+    "url": "/static/css/main.b02d6cc7.chunk.css"
+  },
+  {
+    "revision": "5d7fa4d1af4a158b262d",
+    "url": "/static/js/2.991ef3f6.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/static/js/2.991ef3f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a1f6b982c6feb6a9433",
+    "url": "/static/js/main.4349f68c.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
